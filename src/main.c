36e4a0439a278693acba6276eb7ca7ad4c40a7ab
miketeow <27071053+miketeow@users.noreply.github.com>
2025-05-19 19:30:10 +0800

    refactor and add exit

--- src/main.c
+++ src/main.c
@@ -2,6 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_ARGS 10
+
+struct InputStruct{
+  char command[100];
+  char *arg[MAX_ARGS];
+  int arg_counts;
+};
+
+void handleExitCommand(struct InputStruct* input_args ){
+  exit(0);
+};
+
+struct InputStruct parse_input(char* original_input){
+  struct InputStruct result;
+  result.command[0] = '\0';
+  result.arg[0] = NULL;
+
+  char *tkn;
+  int token_index = 0;
+
+  tkn = strtok(original_input," ");
+  while (tkn!= NULL){
+    //debug
+    // printf("Token: %s\n",tkn);
+
+    if(token_index == 0){
+      // This would be the first token, will be treated as command
+      strncpy(result.command,tkn,sizeof(result.command)-1);
+      // Ensure null terminator
+      result.command[sizeof(result.command) - 1] = '\0';
+      result.arg_counts = 0;
+    } else {
+      if (result.arg_counts < MAX_ARGS){
+        result.arg[result.arg_counts] = tkn;
+        result.arg_counts++;
+      } else {
+        printf("Too many arguments");
+        break;
+      }
+    }
+    token_index++;
+    tkn = strtok(NULL," ");
+  }
+  return result;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -11,6 +57,7 @@ int main(int argc, char *argv[]) {
     printf("$ ");
 
     // Wait for user input
+    // struct InputStruct input;
     char input[100];
 
     if(fgets(input, 100, stdin) != NULL){
@@ -19,7 +66,15 @@ int main(int argc, char *argv[]) {
       if(len > 0 && input[len - 1] == '\n'){
         input[len - 1] = '\0';
       }
-      printf("%s: command not found\n", input);
+
+      struct InputStruct parsed = parse_input(input);
+      if(parsed.command[0] != '\0'){
+        if(strcmp(parsed.command,"exit") == 0){
+          handleExitCommand(&parsed);
+        } else {
+          printf("%s: command not found\n",parsed.command);
+        }
+      }
     } else {
       printf("Input error\n");
       return 1;
