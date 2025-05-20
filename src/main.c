be0bd3f1b6f3805eaacf30498087387939fb54fb
miketeow <27071053+miketeow@users.noreply.github.com>
2025-05-20 19:18:35 +0800

    add type

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@
 
 struct InputStruct{
   char command[100];
-  char *arg[MAX_ARGS];
+  char *args[MAX_ARGS];
   int arg_counts;
 };
+
+const char *builtin_command[] = {"exit","echo","type","a"};
 
 void handleExitCommand(struct InputStruct* input_args ){
   exit(0);
@@ -16,16 +18,26 @@ void handleExitCommand(struct InputStruct* input_args ){
 
 void handleEchoCommand(struct InputStruct* input_args){
   for(int i = 0; i < input_args->arg_counts; i ++){
-    printf("%s ",input_args->arg[i]);
+    printf("%s ",input_args->args[i]);
     if(i == input_args->arg_counts - 1){
       printf("\n");
     }
   }
 }
+
+void handleTypeCommand(struct InputStruct* input_args){
+ for(int i = 0; i < (sizeof(builtin_command)/(sizeof(char*))); i++){
+  if(strcmp(input_args->args[0],builtin_command[i]) == 0){
+    printf("%s is a shell builtin\n",input_args->args[0]);
+    return;
+  }
+ }
+ printf("%s: command not found\n",input_args->args[0]);
+}
 struct InputStruct parse_input(char* original_input){
   struct InputStruct result;
   result.command[0] = '\0';
-  result.arg[0] = NULL;
+  result.args[0] = NULL;
 
   char *tkn;
   int token_index = 0;
@@ -43,7 +55,7 @@ struct InputStruct parse_input(char* original_input){
       result.arg_counts = 0;
     } else {
       if (result.arg_counts < MAX_ARGS){
-        result.arg[result.arg_counts] = tkn;
+        result.args[result.arg_counts] = tkn;
         result.arg_counts++;
       } else {
         printf("Too many arguments");
@@ -81,7 +93,9 @@ int main(int argc, char *argv[]) {
           handleExitCommand(&parsed);
         } else if(strcmp(parsed.command, "echo") == 0){
           handleEchoCommand(&parsed);
-        }else {
+        } else if(strcmp(parsed.command,"type") == 0){
+          handleTypeCommand(&parsed);
+        } else {
           printf("%s: command not found\n",parsed.command);
         }
       }
